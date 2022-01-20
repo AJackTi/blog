ab784aea8294901ec423ce6afcc7ad261709014d
AjackTi <dtrong97vn@gmail.com>
2022-01-20 17:53:05 +0700

    First commit

---
+++ query/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+const post = {}
+
+app.get('/posts', (req, res) => {
+
+})
+
+app.post('/events', (res, req) => {
+
+})
+
+app.listen(4002, ()=>{
+    console.log('Listening on 4002')
+})
