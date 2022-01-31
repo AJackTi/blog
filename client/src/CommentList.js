e1855232001a27afd4b413541402e74efd2a62d7
ajackti <dtrong97vn@gmail.com>
2022-01-31 23:29:08 +0700

    040 Handling Moderation

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,24 +1,31 @@
 import React, {useState, useEffect} from "react";
-import axios from "axios";
 
-export default ({postId}) => {
-    const [comments, setComments] = useState([])
+// export default ({postId}) => {
+//     const [comments, setComments] = useState([])
+//
+//     const fetchData = async () => {
+//         const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`)
+//
+//         setComments(res.data)
+//     }
+//
+//     useEffect(() => {
+//         fetchData()
+//     }, [])
+//
+//     const renderedComments = comments.map(comment => {
+//         return <li key={comment.id}>{comment.content}</li>
+//     })
+//
+//     return <ul>
+//         {renderedComments}
+//     </ul>
+// }
 
-    const fetchData = async () => {
-        const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`)
-
-        setComments(res.data)
-    }
-
-    useEffect(() => {
-        fetchData()
-    }, [])
-
+export default ({comments}) => {
     const renderedComments = comments.map(comment => {
-        return <li key={comment.id}>{comment.content}</li>
+        return <li key={comment.id}>{comment.content}</li>;
     })
 
-    return <ul>
-        {renderedComments}
-    </ul>
+    return <ul>{renderedComments}</ul>;
 }
