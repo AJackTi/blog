2cc897571bd869b0558d01a9e65973f4ac7a6b66
AJackTi <dtrong97vn@gmail.com>
2022-02-05 18:38:17 +0700

    02 A Mini-Microservices App

--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const axios = require('axios')
 
 const app = express()
 app.use(bodyParser.json())
@@ -8,15 +9,9 @@ app.use(cors())
 
 const posts = {}
 
-app.get('/posts', (req, res) => {
-    res.send(posts)
-})
-
-app.post('/events', (req, res) => {
-    const {type, data} = req.body;
-
+const handleEvent = (type, data) => {
     if (type === 'PostCreated') {
-        const {id, title} = req.body;
+        const {id, title} = data;
 
         posts[id] = {id, title, comments: []}
     }
@@ -28,9 +23,41 @@ app.post('/events', (req, res) => {
         post.comments.push({id, content, status})
     }
 
+    if (type === 'CommentUpdated') {
+        const {id, content, postId, status} = data;
+
+        const post = posts[postId];
+        const comment = post.comments.find(comment => {
+            return comment.id === id;
+        })
+
+        comment.status = status
+        comment.content = content;
+    }
+}
+
+app.get('/posts', (req, res) => {
+    res.send(posts)
+})
+
+app.post('/events', (req, res) => {
+    const {type, data} = req.body;
+
+    handleEvent(type, data)
+
+    console.log(posts)
+
     res.send({})
 })
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
     console.log('Listening on 4002')
+
+    const res = await axios.get('http://localhost:4005/events')
+
+    for (let event of res.data) {
+        console.log('Processing event: ', event.type)
+
+        handleEvent(event.type, event.data)
+    }
 })
